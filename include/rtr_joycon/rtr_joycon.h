2153fab29dd2a85f9d746ed782a7725a315d022e
calm0815 <calm0815@example.com>
2020-04-10 18:49:49 +0900

    remake

--- include/rtr_joycon/rtr_joycon.h
+++ include/rtr_joycon/rtr_joycon.h
@@ -5,6 +5,7 @@
 #include <trajectory_msgs/JointTrajectory.h>
 #include <geometry_msgs/Twist.h>
 #include <sensor_msgs/Joy.h>
+#include <std_msgs/Bool.h>
 #include <jog_msgs/JogJoint.h>
 #include <jog_msgs/JogFrame.h>
 
@@ -13,6 +14,7 @@ class RTRJoycon
 public:
   RTRJoycon(ros::NodeHandle& nh);
   void publish(void); // 実行関数
+  void updateCommand(void);
 
 private:
   ros::NodeHandle nh_;
@@ -22,16 +24,21 @@ private:
   ros::Publisher jog_frame_pub_;  // jog_frame用publisher
   ros::Publisher trajectory_pub_;    // JointTrajectory用publisher
 
+  ros::Publisher L1_enable_pub_;
+  ros::Publisher R1_enable_pub_;
+  ros::Publisher mani_arm_enable_pub_;
+
   sensor_msgs::Joy joy_msg_;  // joy_nodeコマンド格納用
 
-  std::vector<std::string> joy_con_key_names_;
   std::vector<std::string> double_arm_joint_names_;     // yamlファイルから読み取ったdouble_armのjoint_name
   std::vector<std::string> tohoku_gipper_joint_names_;  // yamlファイルから読み取ったtohoku_gipperのjoint_name
   std::vector<std::string> mani_gipper_joint_names_;    // yamlファイルから読み取ったmani_gipperのjoint_name
 
-  const float speed_gain_ = 0.2;    // 速度ゲイン
-  int key_config_index_;            // キー設定のindex
-  std::vector<std::string> key_config_;   // yamlファイルから読み取ったキー設定
+  float speed_gain_;    // 速度ゲイン
+
+  std_msgs::Bool L1_enable_;
+  std_msgs::Bool R1_enable_;
+  std_msgs::Bool mani_arm_enable_;
 
   geometry_msgs::Twist cmd_vel_;  // 指令値[タイヤ]
   jog_msgs::JogJoint jog_joint_;  // 指令値[ウデ]
@@ -41,9 +48,7 @@ private:
 
 private:
   void updateJoyMsg(const sensor_msgs::Joy& joy_msg);           // メンバ変数joy_msg_の更新関数（callback）
-  std::vector<std::string> readYaml(const std::string path);    // pathに該当するyaml内の値を返す関数
-  bool setKeyConfig(const int config_num);                      // yamlに記述されたキー設定をkey_config_にセットするmethod
-  float jogCommandSet(const std::string joint_name);
+  bool setCommand(std::string task, float positive, float negative);
 
   template<typename T>
   int getIndexFromVector(std::vector<T> vec, const T target)
@@ -55,6 +60,18 @@ private:
     return index;
   }
 
+  template<typename TYPE>
+  TYPE readYaml(const std::string path)
+  {
+    TYPE param;
+    if (!nh_.getParam(path, param))
+    {
+      std::string error_str = "Failed to get parameter[" + path + "] from yaml file.";
+      ROS_ERROR_STREAM(error_str);
+    }
+    return param;
+  }
+
 };
 
-#endif  // RTR_JOYCON_H
+#endif  // RTR_JOYCON_
